a64851dde93630123158888c2446f2765b229e81
sslukess <luke.sampsonsearle@fitlg.com>
2023-05-29 16:00:23 +1000

    fix typing on payload

--- utils/openAiStream.tsx
+++ utils/openAiStream.tsx
@@ -15,12 +15,12 @@ export interface OpenAIStreamPayload {
     model: string;
     messages: ChatGPTMessage[];
     temperature: number;
-    top_p: number;
-    frequency_penalty: number;
-    presence_penalty: number;
-    max_tokens: number;
-    stream: boolean;
-    n: number;
+    top_p?: number;
+    frequency_penalty?: number;
+    presence_penalty?: number;
+    max_tokens?: number;
+    stream?: boolean;
+    n?: number;
 }
 
 export async function OpenAIStream(payload: OpenAIStreamPayload) {
