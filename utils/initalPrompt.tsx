8f0571867a7e317811a0b200b8716e3954f07dd3
DomGucci <135011278+DomGucci@users.noreply.github.com>
2023-05-30 11:18:41 +0200

    changing URL generation to provide a multi-destination flight route between suggested destinations for each itinerary.

--- utils/initalPrompt.tsx
+++ utils/initalPrompt.tsx
@@ -23,7 +23,9 @@ function generateInitialPrompt() {
         2. Provide a basic list of popular things to do in the city.
         3. Provide a suggestion for the best place restaurant to eat dinner each night. 
         4. How to travel to the city from the previous location. 
-        5. A URL that has the format www.google.com/search?q=hotels+in+x, where x is the city 
+
+        For each itinerary I do the following:
+        If the user has specified a date they wish to start their trip, I include A URL that has the format https://mytrip.travelworld.com/home?tripType=ONLY_FLIGHT&submit=true&multiTransport=true&distribution=1-0&lang=en&displayCurrency=EUR&connections=destinationlist&directSubmit=true where destinationlist is a comma separated list constructed using the destinations I suggest and dates based on the start date of the trip. Each entry of destinationlist should correspond to a destination I suggested and take the form "Destination::<airport code of closest airport to the destination you chose>$<date of arrival>$0". Text within <> is to be changed by me. All other text must stay the same. The final entry of destinationlist should not end with a comma.
          `
     }
   }
